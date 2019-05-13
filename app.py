30e249f3e895e886f3acdde64effc3898cf478df
nkielsky <nathan.kielsky@am.sony.com>
2019-05-13 14:41:32 -0700

    app.py changes   HostName 52.88.255.171

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 #app.py
 #intro to python Linux Academy
+
+def hello_world():
+    name = 'Nathan Kielsky'
+    age = '26'
+    print('My name is ' +name + ' and I am ' + age + ' years old!')
+
+hello_world()
